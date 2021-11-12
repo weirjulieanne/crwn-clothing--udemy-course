68edda87d0f5794c2ccd81be4b70f1cc4bb3f0fe
weirjulieanne <85594164+weirjulieanne@users.noreply.github.com>
2021-11-11 18:43:34 -0700

    added routes and switch, added history to urls

--- crown-app/src/App.js
+++ crown-app/src/App.js
@@ -1,8 +1,23 @@
+import React from "react";
+import { Route, Switch } from "react-router-dom";
 import "./App.css";
 import HomePage from "./pages/home-page/homepage.component";
 
+const HatsPage = () => (
+  <div>
+    <h1>HATS PAGE</h1>
+  </div>
+);
+
 function App() {
-  return <HomePage />;
+  return (
+    <div>
+      <Switch>
+        <Route exact path="/" component={HomePage} />
+        <Route exact path="/shop/hats" component={HatsPage} />
+      </Switch>
+    </div>
+  );
 }
 
 export default App;
