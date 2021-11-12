b1039a39268b9769fe1928b5d156df0507e71ff5
weirjulieanne <85594164+weirjulieanne@users.noreply.github.com>
2021-11-11 21:07:35 -0700

    added header and styling

--- crown-app/src/App.js
+++ crown-app/src/App.js
@@ -3,10 +3,12 @@ import { Route, Switch } from "react-router-dom";
 import "./App.css";
 import HomePage from "./pages/home-page/homepage.component";
 import ShopPage from "./pages/shop-page/shop.component";
+import Header from "./components/header/header.component";
 
 function App() {
   return (
     <div>
+      <Header />
       <Switch>
         <Route exact path="/" component={HomePage} />
         <Route exact path="/shop" component={ShopPage} />
